aea83655e003cc54ee381007d49fb6aa7cf24f82
tanya.pog <tanya.pog@emms.ru>
2020-05-08 17:16:28 +0300

    see #7 packages lint and effective_dart for neat code have been installed

--- lib/domain/core/value_validatores.dart
+++ lib/domain/core/value_validatores.dart
@@ -3,15 +3,17 @@ import 'failures.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const String emailRegex = r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
-  if (RegExp(emailRegex).hasMatch(input))
+  if (RegExp(emailRegex).hasMatch(input)) {
     return right(input);
-  else
+  } else {
     return left(ValueFailure.invalidEmail(failedValue: input));
+  }
 }
 
 Either<ValueFailure<String>, String> validatePassword(String input) {
-  if (input.length >= 6)
+  if (input.length >= 6) {
     return right(input);
-  else
+  } else {
     return left(ValueFailure.shortPassword(failedValue: input));
+  }
 }
