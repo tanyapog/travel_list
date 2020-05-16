3e9e31656adc62dd5014c521ced6bae2734d0548
tanya.pog <tanya.pog@emms.ru>
2020-05-16 23:42:31 +0300

    see #6 Auth BLoC

---
+++ lib/application/auth/auth_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:travel_list/domain/auth/i_auth_facade.dart';
+import 'package:travel_list/domain/auth/user.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+part 'auth_bloc.freezed.dart';
+
+@injectable
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final IAuthFacade _authFacade;
+
+  AuthBloc(this._authFacade);
+
+  @override
+  AuthState get initialState => const AuthState.initial();
+
+  @override
+  Stream<AuthState> mapEventToState(AuthEvent event) async* {
+    yield* event.map(
+      authCheckRequested: (e) async* {
+        final userOption = await _authFacade.getSignedInUser();
+        yield userOption.fold(
+          () => const AuthState.unauthenticated(),
+          (user) => AuthState.authenticated(user),
+        );
+      },
+      signedOut: (e) async* {
+        await _authFacade.signOut();
+        yield const AuthState.unauthenticated();
+      }
+    );
+  }
+}
--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -10,6 +10,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:travel_list/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:travel_list/domain/auth/i_auth_facade.dart';
 import 'package:travel_list/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:travel_list/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
@@ -21,6 +22,7 @@ void $initGetIt(GetIt g, {String environment}) {
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
+  g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
 }
 
 class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
