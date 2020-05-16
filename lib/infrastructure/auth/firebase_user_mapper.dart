a33c15c712664a406c255f72ac235355c5f4c4e3
tanya.pog <tanya.pog@emms.ru>
2020-05-16 23:39:10 +0300

    see #6 extension .toDomain for FirebaseUser that do map FirebaseUser to project's User. Extensions was introduced in Dart 2.7 so version of Dart sdk was upgraded to 2.8.0. Other libraries was updated in concern.

---
+++ lib/infrastructure/auth/firebase_user_mapper.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travel_list/domain/auth/user.dart';
+import 'package:travel_list/domain/auth/value_objects.dart';
+import 'package:travel_list/domain/core/value_objects.dart';
+
+extension FirebaseUserMapper on FirebaseUser {
+  User toDomain() {
+    return User(
+      id: UniqueId.fromUniqueString(uid),
+      name: displayName,
+      email: Email(email),
+    );
+  }
+}
