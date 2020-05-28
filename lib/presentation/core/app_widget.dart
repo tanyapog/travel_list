d659e2b6ade9c7abf618dda7d58e99920d9daf2f
tanya.pog <tanya.pog@emms.ru>
2020-05-28 12:30:22 +0300

    see #6 The main widget should be wrapped in BlocProvider<AuthBloc> to check authentication at once while starting the app.

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,22 +1,29 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_list/application/auth/auth_bloc.dart';
+import 'package:travel_list/injection.dart';
 import 'package:travel_list/presentation/pages/sign_in/sign_in_page.dart';
 import 'package:travel_list/presentation/routes/router.gr.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Travel List',
-      home: SignInPage(),
-      builder: ExtendedNavigator<Router>(router: Router(),),
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-        )
+    return BlocProvider(
+      create: (context) =>
+        getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+      child: MaterialApp(
+        title: 'Travel List',
+        home: SignInPage(),
+        builder: ExtendedNavigator<Router>(router: Router(),),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.lime,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          )
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
