e4e742b4756c648da9508e4a715d0c72eadccbaf
tanya.pog <tanya.pog@emms.ru>
2020-05-28 11:55:24 +0300

    see #6 some comments

--- lib/application/auth/auth_event.dart
+++ lib/application/auth/auth_event.dart
@@ -2,6 +2,8 @@ part of 'auth_bloc.dart';
 
 @freezed
 abstract class AuthEvent with _$AuthEvent {
+  // event to notify the bloc that it needs to check if the user is currently authenticated or not
   const factory AuthEvent.authCheckRequested() = AuthCheckRequested;
+  // event to notify the bloc that the user has successfully logged out
   const factory AuthEvent.signedOut() = SignedOut;
 }
