d6bb5aa4d03928b04c9bb7248ad7eaa8a37559f6
tanya.pog <tanya.pog@emms.ru>
2020-05-29 18:35:35 +0300

    see #6 WidgetsFlutterBinding.ensureInitialized() is required in Flutter v1.9.4+ before using any plugins if the code is executed before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:travel_list/injection.dart';
 import 'package:travel_list/presentation/core/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
   runApp(AppWidget());
 }
