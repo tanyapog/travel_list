adbc53b709dcbe2767f5281b14a29d3ee80a856d
tanya.pog <tanya.pog@emms.ru>
2020-06-01 18:05:56 +0300

    see #6 Some dependencies and deprecate code was updated

--- lib/infrastructure/core/firebase_injectable_module.dart
+++ lib/infrastructure/core/firebase_injectable_module.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 
-@registerModule
+@module
 abstract class FirebaseInjectableModule {
   @lazySingleton
   GoogleSignIn get googleSignIn => GoogleSignIn();
