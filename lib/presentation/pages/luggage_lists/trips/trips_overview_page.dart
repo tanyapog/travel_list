7ead387737da101a48f51c8861456237f7f74db3
tanya.pog <tanya.pog@emms.ru>
2020-06-04 18:39:18 +0300

    see #6 Now user is provided in trips_overview_page.dart and we could finally show actual user information in the sidebar. The installing of Provider package mistakenly got into the previous commit.

--- lib/presentation/pages/luggage_lists/trips/trips_overview_page.dart
+++ lib/presentation/pages/luggage_lists/trips/trips_overview_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:travel_list/application/auth/auth_bloc.dart';
+import 'package:travel_list/domain/auth/user.dart';
+import 'package:travel_list/domain/core/errors.dart';
 import 'package:travel_list/models/trip.dart';
 import 'package:travel_list/presentation/pages/luggage_lists/trips/widgets/sidebar_drawer.dart';
 import 'package:travel_list/shared/db_imitation.dart';
@@ -23,36 +28,42 @@ class TripsOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: SidebarDrawer(),
-      appBar: AppBar(
-        title: const Text('Packing Helper'), // todo nice application name
+    return Provider<User>.value(
+      value: context.bloc<AuthBloc>().state.maybeWhen(
+        authenticated: (user) => user,
+        orElse: () => throw UnexpectedValueError, // todo access deny terminal error
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          newTravelListBtn(context),
-          Expanded(
-            child: ListView.builder(
-              itemCount: DbImitation.trips.length,
-              itemBuilder: (context, i) {
-                final Trip _trip = DbImitation.trips[i];
-                return Container(
-                  height: 50,
-                  color: Colors.amber[50*(i+1)],
-                  child: Center(
-                    child: InkWell(
-                      onTap: () {
+      child: Scaffold(
+        drawer: SidebarDrawer(),
+        appBar: AppBar(
+          title: const Text('Packing Helper'), // todo nice application name
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            newTravelListBtn(context),
+            Expanded(
+              child: ListView.builder(
+                itemCount: DbImitation.trips.length,
+                itemBuilder: (context, i) {
+                  final Trip _trip = DbImitation.trips[i];
+                  return Container(
+                    height: 50,
+                    color: Colors.amber[50*(i+1)],
+                    child: Center(
+                      child: InkWell(
+                        onTap: () {
 //                        Navigator.of(context).pushNamed(luggageListRoute, arguments: _trip);
-                      },
-                      child: Text(_trip.name),
-                    )
-                  ),
-                );
-              }
-            )
-          ),
-        ],
+                        },
+                        child: Text(_trip.name),
+                      )
+                    ),
+                  );
+                }
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
