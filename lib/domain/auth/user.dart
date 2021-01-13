43f0ed2c5909ef0ecb41d130d96a895c14d345c6
tanya.pog <tanya.pog@emms.ru>
2021-01-13 17:36:12 +0300

    see #6 Got rid of Entity cause it is redundant

--- lib/domain/auth/user.dart
+++ lib/domain/auth/user.dart
@@ -2,13 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:meta/meta.dart';
 import 'package:travel_list/domain/auth/value_objects.dart';
-import 'package:travel_list/domain/core/entity.dart';
 import 'package:travel_list/domain/core/value_objects.dart';
 
 part 'user.freezed.dart';
 
 @freezed
-abstract class User with _$User implements IEntity {
+abstract class User with _$User {
   const factory User({
     @required UniqueId id,
     @required StringSingleLine name,
--- lib/domain/core/entity.dart
+++
@@ -1,5 +0,0 @@
-import 'package:travel_list/domain/core/value_objects.dart';
-
-abstract class IEntity {
-  UniqueId get id;
-}
