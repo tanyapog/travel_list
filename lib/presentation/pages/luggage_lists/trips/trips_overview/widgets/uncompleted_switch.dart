d0890f82a74974904bc4b04396ba65cedddabe4c
tanya.pog <tanya.pog@emms.ru>
2021-01-27 19:36:14 +0300

    see #1 UncompletedSwitch with Icons, animated using hooks.

--- lib/presentation/pages/luggage_lists/trips/trips_overview/widgets/uncompleted_switch.dart
+++ lib/presentation/pages/luggage_lists/trips/trips_overview/widgets/uncompleted_switch.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:travel_list/application/trips/trip_watcher/trip_watcher_bloc.dart';
 
-class UncompletedSwitch extends StatefulWidget {
-  @override
-  _UncompletedSwitchState createState() => _UncompletedSwitchState();
-}
-
-class _UncompletedSwitchState extends State<UncompletedSwitch> {
-  bool toggle = false;
-
+class UncompletedSwitch extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final toggleState = useState(false);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Checkbox(
-        checkColor: Colors.grey,
-        activeColor: Colors.white70,
-        value: toggle,
-        onChanged: (bool value) {
-          setState(() {
-            context.bloc<TripWatcherBloc>().add(toggle
-              ? const TripWatcherEvent.watchAllStarted()
-              : const TripWatcherEvent.watchUncompletedStarted()
-            );
-            toggle = value;
-          });
+      child: InkResponse(
+        onTap: () {
+          toggleState.value = !toggleState.value;
+          context.bloc<TripWatcherBloc>().add(toggleState.value
+              ? const TripWatcherEvent.watchUncompletedStarted()
+              : const TripWatcherEvent.watchAllStarted(),
+          );
         },
+        child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 100),
+            transitionBuilder: (child, animation) => ScaleTransition(
+              scale: animation,
+              child: child,),
+            child: toggleState.value
+            // without keys flutter thinks that icons are identical and animation doesn't work
+                ? Icon(Icons.check_box_outline_blank, key: const Key('outline'),)
+                : Icon(Icons.indeterminate_check_box, key: const Key('indeterminate'))
+        ),
       ),
     );
   }
