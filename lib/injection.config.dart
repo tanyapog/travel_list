c3cc5f54b15fc330b0cdf3706cdce04ccf8d7262
tanya.pog <tanya.pog@emms.ru>
2021-01-27 23:02:25 +0300

    Injectable is updated to v1.0.4

---
+++ lib/injection.config.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'application/auth/auth_bloc.dart';
+import 'infrastructure/auth/firebase_auth_facade.dart';
+import 'infrastructure/core/firebase_injectable_module.dart';
+import 'domain/auth/i_auth_facade.dart';
+import 'domain/trips/i_trip_repository.dart';
+import 'application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'application/trips/trip_actor/trip_actor_bloc.dart';
+import 'application/trips/trip_form/trip_form_bloc.dart';
+import 'infrastructure/trips/trip_repository.dart';
+import 'application/trips/trip_watcher/trip_watcher_bloc.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
+
+GetIt $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) {
+  final gh = GetItHelper(get, environment, environmentFilter);
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  gh.lazySingleton<FirebaseAuth>(() => firebaseInjectableModule.firebaseAuth);
+  gh.lazySingleton<Firestore>(() => firebaseInjectableModule.firestore);
+  gh.lazySingleton<GoogleSignIn>(() => firebaseInjectableModule.googleSignIn);
+  gh.lazySingleton<IAuthFacade>(
+      () => FirebaseAuthFacade(get<FirebaseAuth>(), get<GoogleSignIn>()));
+  gh.lazySingleton<ITripRepository>(() => TripRepository(get<Firestore>()));
+  gh.factory<SignInFormBloc>(() => SignInFormBloc(get<IAuthFacade>()));
+  gh.factory<TripActorBloc>(() => TripActorBloc(get<ITripRepository>()));
+  gh.factory<TripFormBloc>(() => TripFormBloc(get<ITripRepository>()));
+  gh.factory<TripWatcherBloc>(() => TripWatcherBloc(get<ITripRepository>()));
+  gh.factory<AuthBloc>(() => AuthBloc(get<IAuthFacade>()));
+  return get;
+}
+
+class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
