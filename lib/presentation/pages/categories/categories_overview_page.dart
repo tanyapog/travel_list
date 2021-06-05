d37600f717ac94eb216a80a273743bc12887f256
tanya.pog <tanya.pog@emms.ru>
2021-06-05 16:05:48 +0300

    see #23 List of categories on CategoriesOverviewPage

--- lib/presentation/pages/categories/categories_overview_page.dart
+++ lib/presentation/pages/categories/categories_overview_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_list/application/categories/category_watcher/category_watcher_bloc.dart';
+import 'package:travel_list/injection.dart';
+import 'package:travel_list/presentation/pages/categories/widgets/critical_failure_display.dart';
+import 'package:travel_list/presentation/pages/navigation/navigation_drawer.dart';
 
 class CategoriesOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Categories overview'))
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<CategoryWatcherBloc>()
+            ..add(const CategoryWatcherEvent.watchAllStarted()),
+        ),
+      ],
+      child: Scaffold(
+        drawer: NavigationDrawer(),
+        appBar: AppBar(
+          title: const Text('Categories'),
+        ),
+        body: BlocBuilder<CategoryWatcherBloc, CategoryWatcherState>(
+          builder: (context, state) => state.map(
+            initial: (_) => Container(),
+            loadInProgress: (_) => const Center(child: CircularProgressIndicator()),
+            loadSuccess: (state) => ListView.builder(
+              itemBuilder: (context, index) {
+                final category = state.categories[index];
+                return Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Text(
+                      "${category.position}. ${category.name}",
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                );
+              },
+              itemCount: state.categories.length,
+            ),
+            loadFailure: (state) => CriticalFailureDisplay(failure: state.categoryFailure),
+            reordered: (state) => ListView.builder(
+              itemBuilder: (context, index) {
+                final category = state.categories[index];
+                return Center(child: Text('Reordered category ${category.name}'),);
+              },
+              itemCount: state.categories.length,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentation/pages/categories/widgets/critical_failure_display.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:travel_list/domain/categories/category_failure.dart';
+
+class CriticalFailureDisplay extends StatelessWidget {
+  final CategoryFailure failure;
+
+  const CriticalFailureDisplay({Key key, @required this.failure}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Icon(Icons.report_problem, size: 76.0, color: Colors.red,),
+          Text(
+            failure.maybeMap(
+                insufficientPermission: (_) => 'Insufficient permissions',
+                orElse: () => 'Unexpected error. \nPlease contact support'),
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
