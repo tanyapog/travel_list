fafef6d809302d55324ecbda057fd10158e60bd2
tanya.pog <tanya.pog@emms.ru>
2021-06-08 15:26:54 +0300

    linting

--- lib/presentation/pages/trips/trips_overview/widgets/trip_card.dart
+++ lib/presentation/pages/trips/trips_overview/widgets/trip_card.dart
@@ -9,10 +9,7 @@ import 'package:travel_list/presentation/routes/router.gr.dart';
 class TripCard extends StatelessWidget {
   final Trip trip;
 
-  const TripCard({
-    Key key,
-    @required this.trip,
-  }) : super(key: key);
+  const TripCard({Key key, @required this.trip,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
