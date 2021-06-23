e8d94c0c4b5ccb6de8fa67693f22e8b46fe9940b
tanya.pog <tanya.pog@emms.ru>
2021-06-23 19:35:00 +0300

    see #1 #25 bugfix: deletion error message didn't show

--- lib/presentation/pages/trips/trip_form/widgets/trip_deletion_dialog.dart
+++ lib/presentation/pages/trips/trip_form/widgets/trip_deletion_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_list/application/trips/trip_actor/trip_actor_bloc.dart';
@@ -13,38 +14,51 @@ class TripDeletionDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
+    return BlocProvider<TripActorBloc>(
       create: (context) => getIt<TripActorBloc>(),
       child: customDialog(
-        child: Column(
-          mainAxisSize: MainAxisSize.min, // To make the card compact
-          children: <Widget>[
-            const Text("Deleting the trip",
-              style: TextStyle(fontWeight: FontWeight.bold),),
-            const SizedBox(height: 10),
-            ListTile(
-              title: Text(trip.name.getOrCrash()),
-              subtitle: Text('${trip.description.getOrCrash()} \nдек. 2020'),
-              isThreeLine: true,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                greyButton(onPressed: () => Navigator.of(context).pop(), title: "CANCEL"),
-                const SizedBox(width: 10),
-                BlocBuilder<TripActorBloc, TripActorState>(
-                  builder: (context, state) => amberButton(
-                    onPressed: () {
-                      context.read<TripActorBloc>().add(TripActorEvent.deleted(trip));
-                      Navigator.of(context).pop();
-                    },
-                    title: "DELETE"
-                  ),
-                ),
-              ],
-            ),
-          ],
+        child: BlocListener<TripActorBloc, TripActorState>(
+          listener: (context, state) {
+            state.maybeMap(
+              deleteFailure: (state) {
+                FlushbarHelper.createError(
+                  duration: const Duration(seconds: 5),
+                  message: state.tripFailure.message,
+                ).show(context);
+              },
+              orElse: () {},
+            );
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              const Text("Deleting the trip",
+                style: TextStyle(fontWeight: FontWeight.bold),),
+              const SizedBox(height: 10),
+              ListTile(
+                title: Text(trip.name.getOrCrash()),
+                subtitle: Text('${trip.description.getOrCrash()} \nдек. 2020'),
+                isThreeLine: true,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  greyButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    title: "CANCEL"),
+                  const SizedBox(width: 10),
+                  BlocBuilder<TripActorBloc, TripActorState>(
+                    builder: (context, state) => amberButton(
+                      onPressed: () {
+                        context.read<TripActorBloc>().add(TripActorEvent.deleted(trip));
+                        Navigator.of(context).pop();
+                      },
+                      title: "DELETE"),),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/pages/trips/trips_overview/trips_overview_page.dart
+++ lib/presentation/pages/trips/trips_overview/trips_overview_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:travel_list/application/trips/trip_actor/trip_actor_bloc.dart';
 import 'package:travel_list/application/trips/trip_watcher/trip_watcher_bloc.dart';
 import 'package:travel_list/injection.dart';
 import 'package:travel_list/presentation/pages/navigation/navigation_drawer.dart';
@@ -14,43 +12,21 @@ class TripsOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<TripWatcherBloc>(
-          create: (context) => getIt<TripWatcherBloc>()
-            ..add(const TripWatcherEvent.watchAllStarted()),),
-        BlocProvider<TripActorBloc>(
-          create: (context) => getIt<TripActorBloc>(),),
-      ],
-      child: MultiBlocListener(
-        listeners: [
-          BlocListener<TripActorBloc, TripActorState>(
-            listener: (context, state) {
-              state.maybeMap(
-                deleteFailure: (state) {
-                  FlushbarHelper.createError(
-                    duration: const Duration(seconds: 5),
-                    message: state.tripFailure.message
-                  ).show(context);
-                },
-                orElse: () {},
-              );
-            },
-          ),
-        ],
-        child: Scaffold(
-          drawer: NavigationDrawer(),
-          appBar: AppBar(
-            title: const Text('My trips'),
-            actions: <Widget>[
-              UncompletedSwitch(),
-            ],
-          ),
-          body: TripsOverviewBody(),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => ExtendedNavigator.of(context).pushTripFormPage(trip: null),
-            child: const Icon(Icons.add),
-          ),
+    return BlocProvider<TripWatcherBloc>(
+      create: (context) => getIt<TripWatcherBloc>()
+        ..add(const TripWatcherEvent.watchAllStarted()),
+      child: Scaffold(
+        drawer: NavigationDrawer(),
+        appBar: AppBar(
+          title: const Text('My trips'),
+          actions: <Widget>[
+            UncompletedSwitch(),
+          ],
+        ),
+        body: TripsOverviewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => ExtendedNavigator.of(context).pushTripFormPage(trip: null),
+          child: const Icon(Icons.add),
         ),
       ),
     );
