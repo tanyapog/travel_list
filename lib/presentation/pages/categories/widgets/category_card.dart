f96730876530896c0bf5431f1b1a4cbc206175e1
tanya.pog <tanya.pog@emms.ru>
2021-06-27 16:57:57 +0300

    see #23 deletion of category. Though deletion works, RangeError occurs in reorderable list.

--- lib/presentation/pages/categories/widgets/category_card.dart
+++ lib/presentation/pages/categories/widgets/category_card.dart
@@ -1,8 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
 import 'package:travel_list/domain/categories/category.dart';
+import 'package:travel_list/presentation/core/custom_widgets/custom_delete_slide_action.dart';
 import 'package:travel_list/presentation/pages/categories/widgets/category_edit_dialog.dart';
+import 'package:travel_list/presentation/pages/categories/widgets/category_deletion_dialog.dart';
 
 class CategoryCard extends StatelessWidget {
   final Category category;
@@ -23,12 +26,24 @@ class CategoryCard extends StatelessWidget {
         context: context,
         builder: (BuildContext context) => CategoryEditDialog(category: category),
       ),
-      child: Card(
-        elevation: elevation,
-        child: ListTile(
-          title: Text(category.name),
-          trailing: const Handle(
-            child: Icon(Icons.drag_handle, color: Colors.grey,)
+      child: Slidable(
+        actionPane: const SlidableDrawerActionPane(),
+        secondaryActions: [
+          customDeleteSlideAction(
+            onTap: () => showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (context) => CategoryDeletionDialog(category: category),
+            ),
+          ),
+        ],
+        child: Card(
+          elevation: elevation,
+          child: ListTile(
+            title: Text(category.name),
+            trailing: const Handle(
+              child: Icon(Icons.drag_handle, color: Colors.grey,)
+            ),
           ),
         ),
       ),
---
+++ lib/presentation/pages/categories/widgets/category_deletion_dialog.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_list/application/categories/category_actor/category_actor_bloc.dart';
+import 'package:travel_list/domain/categories/category.dart';
+import 'package:travel_list/injection.dart';
+import 'package:travel_list/presentation/core/custom_widgets/custom_buttons.dart';
+import 'package:travel_list/presentation/core/custom_widgets/custom_flushbar_helper.dart';
+import 'package:travel_list/presentation/core/custom_widgets/dialog_box_decoration.dart';
+
+class CategoryDeletionDialog extends StatelessWidget {
+  final Category category;
+
+  const CategoryDeletionDialog({Key key, @required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CategoryActorBloc>(
+      create: (context) => getIt<CategoryActorBloc>(),
+      child: customDialog(
+        child: BlocListener<CategoryActorBloc, CategoryActorState>(
+          listener: (context, state) {
+            state.maybeMap(
+              deleteFailure: (state) => customErrorFlushbar(
+                  message: state.categoryFailure.message).show(context),
+              orElse: () {},
+            );
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              const Text("Deleting a category",
+                style: TextStyle(fontWeight: FontWeight.bold),),
+              const SizedBox(height: 10),
+              ListTile(
+                title: Text(category.name),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  greyButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    title: "CANCEL"),
+                  const SizedBox(width: 10),
+                  BlocBuilder<CategoryActorBloc, CategoryActorState>(
+                    builder: (context, state) => amberButton(
+                      onPressed: () {
+                        context.read<CategoryActorBloc>().add(CategoryActorEvent.deleted(category));
+                        Navigator.of(context).pop();
+                      },
+                      title: "DELETE"),),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
