a0ec6f8fb1dddb6cf508b99868edd08b3356d887
tanyapog <megatatb@yandex.ru>
2021-10-18 21:31:45 +0300

    see #21 package name for auto_route annotations changed; imports was sorted alphabetically; generateNavigationHelperExtension property commented out as it doesn't exist anymore. Will check it later.

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,12 +1,12 @@
-import 'package:auto_route/auto_route_annotations.dart';
+import 'package:auto_route/annotations.dart';
 import 'package:travel_list/presentation/pages/categories/categories_overview_page.dart';
-import 'package:travel_list/presentation/pages/trips/trip_form/trip_form_page.dart';
-import 'package:travel_list/presentation/pages/trips/trips_overview/trips_overview_page.dart';
 import 'package:travel_list/presentation/pages/sign_in/sign_in_page.dart';
 import 'package:travel_list/presentation/pages/splash/splash_page.dart';
+import 'package:travel_list/presentation/pages/trips/trip_form/trip_form_page.dart';
+import 'package:travel_list/presentation/pages/trips/trips_overview/trips_overview_page.dart';
 
 @MaterialAutoRouter(
-  generateNavigationHelperExtension: true,
+  // generateNavigationHelperExtension: true,
   routes: <AutoRoute> [
     MaterialRoute(page: SplashPage, initial: true),
     MaterialRoute(page: SignInPage),
