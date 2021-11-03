dee792f9f94e162533477812b405a9735424d617
tanyapog <megatatb@yandex.ru>
2021-11-03 18:57:10 +0300

    see #21 trailing comma

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -27,7 +27,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       signedOut: (e) async* {
         await _authFacade.signOut();
         yield const AuthState.unauthenticated();
-      }
+      },
     );
   }
 }
