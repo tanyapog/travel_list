6fb857db46c98e95f0752a822e0e1d2bb4c98a4d
tanyapog <megatatb@yandex.ru>
2021-11-11 00:03:11 +0300

    see #14 instrumentation test to run it automatically in Firebase Test Lab through Codemagic

---
+++ android/app/src/androidTest/java/com/example/travel_list/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.travel_list;
+
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import com.example.travel_list.MainActivity;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
