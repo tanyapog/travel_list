21b763e654d432e7d52b83096a0df12a734681b6
tanyapog <megatatb@yandex.ru>
2021-12-18 17:33:31 +0300

    see #14 I saved Codemagic Flutter drive arguments here

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,6 @@
 import 'package:integration_test/integration_test_driver.dart';
 
+// Codemagic Flutter drive arguments:
+// drive --driver=test_driver/integration.dart --target=integration_test/presentation/pages/sign_in/sign_in_page_i_test.dart
+
 Future<void> main() => integrationDriver();
