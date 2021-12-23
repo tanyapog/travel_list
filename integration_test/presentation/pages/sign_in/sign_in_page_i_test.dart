7b4e9bde9decd2a1d04fac945245bbb5481742ad
tanyapog <megatatb@yandex.ru>
2021-12-23 14:50:47 +0300

    see #14 directories structure for integration tests

--- integration_test/presentation/pages/sign_in/sign_in_page_i_test.dart
+++ integration_test/presentation/pages/sign_in/sign_in_page_i_test.dart
@@ -1,27 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:injectable/injectable.dart';
-import 'package:integration_test/integration_test.dart';
-import 'package:travel_list/injection.dart';
 import 'package:travel_list/presentation/core/app_widget.dart';
 import 'package:travel_list/presentation/pages/sign_in/sign_in_page.dart';
 import 'package:travel_list/presentation/pages/trips/trips_overview/trips_overview_page.dart';
 import 'package:travel_list/presentation/pages/trips/trips_overview/widgets/trip_card.dart';
 
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+import '../../../test_globals.dart';
 
-  WidgetsFlutterBinding.ensureInitialized();
-  configureInjection(Environment.prod, NoEnvOrContains(Environment.prod));
-
-  const itUsername = "tl_i_tester";
-  const itEmail = "tl_i_tester@itest.com";
-  const itPassword = "tlItPwd032";
-
-  deleteTestUserIfNeed(itEmail, itPassword);
-
+void signInPageTest() {
   Finder findEmail() => find.bySemanticsLabel('Email');
   Finder findPassword() => find.bySemanticsLabel('Password');
   Finder findRegisterButton() => find.text('REGISTER');
@@ -99,29 +85,4 @@ void main() {
       }
     );
   });
-}
-
-Future <void> deleteTestUserIfNeed(String email, String password) async {
-  await Firebase.initializeApp();
-  final FirebaseAuth _firebaseAuth = getIt<FirebaseAuth>();
-  try {
-    await _firebaseAuth
-        .signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found') {
-      // nothing to do
-      return;
-    } else {print("Can't  login: $e");}
-  } on Exception catch (e) {print("Can't  login: $e");}
-
-  final User? testUser = _firebaseAuth.currentUser;
-  if (testUser != null) {
-    testUser.delete()
-      .then((value) => print("test user deleted"))
-      .catchError((error) => print("Can't  delete test user: $error"));
-    _firebaseAuth.signOut();
-  }
 }
