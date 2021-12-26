e265ac1cb2a99f660b350d4a2f20f92457c882c3
tanyapog <megatatb@yandex.ru>
2021-12-26 21:37:29 +0300

    see #14 test for editing trip added

--- integration_test/presentation/pages/trips/trips_i_test.dart
+++ integration_test/presentation/pages/trips/trips_i_test.dart
@@ -7,6 +7,8 @@ import 'package:travel_list/presentation/pages/trips/trips_overview/widgets/trip
 
 // this test tests both tipsOverviewPage and TripFormPage
 void tripsTest() {
+  const _tripName = 'Norway 2021';
+  const _tripNewName = 'Norway jun, 2021';
 
   group('Overview trips, create, rename and delete trip tests', () {
     testWidgets(
@@ -40,15 +42,37 @@ void tripsTest() {
           expect(formKey.currentState?.validate(), isFalse);
           expect(find.text('Cannot be empty'), findsOneWidget);
 
-          await tester.enterText(find.bySemanticsLabel('name'), 'Norway 2021');
+          await tester.enterText(find.bySemanticsLabel('name'), _tripName);
           await tester.enterText(find.bySemanticsLabel('description'), 'hiking');
           await tester.pumpAndSettle();
 
           await tester.tap(find.byIcon(Icons.check));
           await tester.pumpAndSettle(const Duration(seconds: 1));
           expect(find.byType(TripsOverviewPage), findsOneWidget);
-          expect(find.text('Norway 2021'), findsOneWidget);
+          expect(find.text(_tripName), findsOneWidget);
           expect(find.byKey(const Key('TripDescription')), findsOneWidget);
+        }
+    );
+
+    testWidgets(
+        'Creating a trip with empty name is prohibited. '
+        'After creating trip "Norway 2021" it should appear in list',
+        (WidgetTester tester) async {
+          await tester.pumpWidget(AppWidget());
+          await tester.pumpAndSettle();
+
+          await tester.tap(find.text(_tripName));
+          await tester.pumpAndSettle();
+          expect(find.byType(TripFormPage), findsOneWidget);
+          expect(find.text('Edit a trip'), findsOneWidget);
+
+          await tester.enterText(find.bySemanticsLabel('name'), _tripNewName);
+          await tester.pumpAndSettle();
+
+          await tester.tap(find.byIcon(Icons.check));
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+          expect(find.byType(TripsOverviewPage), findsOneWidget);
+          expect(find.text(_tripNewName), findsOneWidget);
         }
     );
   });
