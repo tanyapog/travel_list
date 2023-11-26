4f20244920af3cae1b3e66fa636a3ef77da12500
tanyapog <megatatb@yandex.ru>
2023-11-26 15:57:09 +0300

    setup for running the app on ios (automatically created by xcode and "flutterfire configure")

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
-import 'package:travel_list/application/travel_list_bloc_observer.dart';
-import 'package:travel_list/global.dart';
-import 'package:travel_list/injection.dart';
 import 'package:travel_list/presentation/core/app_widget.dart';
+import 'application/travel_list_bloc_observer.dart';
+import 'firebase_options.dart';
+import 'global.dart';
+import 'injection.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureInjection(Environment.prod);
-  await Firebase.initializeApp();
   if (useFirebaseEmulator) {
     await connectToEmulator();
   }
